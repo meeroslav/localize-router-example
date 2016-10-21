7d0397e068aaf99d9847c64609c340ea1ae44807
Miroslav Jonas <meeroslav@yahoo.com>
2016-10-21 13:15:11 +0200

    Remove unused references

--- src/app/users/profile/profile.component.ts
+++ src/app/users/profile/profile.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {Observable} from "rxjs";
-import {ActivatedRoute} from "@angular/router";
+import { Component } from '@angular/core';
+import { Observable } from "rxjs";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-profile',
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {Observable} from "rxjs";
+import { Component } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-user',
@@ -8,7 +8,7 @@ import {Observable} from "rxjs";
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
-  user: Observable<string>
+  user: Observable<string>;
 
   constructor(private route: ActivatedRoute) {
     this.user = route.params.map((p: any) => p.id);
