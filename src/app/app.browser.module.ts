b48c381d716037543d5e2b9967eb21d602b1be81
Miroslav Jonas <meeroslav@yahoo.com>
2017-02-26 08:30:32 +0100

    Bring latest changes to example project

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -1,7 +1,8 @@
 import { UniversalModule } from 'angular2-universal';
 import { NgModule } from '@angular/core';
 import { Http } from '@angular/http';
-import { TranslateModule, TranslateLoader, TranslateStaticLoader } from 'ng2-translate';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
@@ -10,7 +11,7 @@ import { LocalizeRouterModule } from 'localize-router';
 import { UsersModule } from './users/users.module';
 
 export function createTranslateLoader(http: Http) {
-  return new TranslateStaticLoader(http, '/assets/locales', '.json');
+  return new TranslateHttpLoader(http, '/assets/locales/', '.json');
 }
 
 const routes: Routes = [
@@ -25,9 +26,11 @@ const routes: Routes = [
   imports: [
     UniversalModule,
     TranslateModule.forRoot({
-      provide: TranslateLoader,
-      useFactory: (createTranslateLoader),
-      deps: [Http]
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [ Http ]
+      }
     }),
     RouterModule.forRoot(routes),
     LocalizeRouterModule.forRoot(routes),
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,18 +3,14 @@ import { RouterModule } from '@angular/router';
 import { UsersComponent } from './users.component';
 import { UserComponent } from './user/user.component';
 import { ProfileComponent } from './profile/profile.component';
-import { CommonModule } from "@angular/common";
-import { TranslateModule } from 'ng2-translate';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 import { LocalizeRouterModule } from 'localize-router';
 
 let routes = [
-  { path: 'users',
-    children: [
-      { path: '', component: UsersComponent },
-      { path: ':id', component: UserComponent },
-      { path: ':id/profile', component: ProfileComponent }
-    ]
-  }
+  { path: '', component: UsersComponent },
+  { path: ':id', component: UserComponent },
+  { path: ':id/profile', component: ProfileComponent }
 ];
 
 @NgModule({
